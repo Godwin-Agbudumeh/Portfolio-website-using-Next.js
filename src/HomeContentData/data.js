16394276135485d6449d1c36ecc609293cb2b3cf
Godwin Agbudumeh <godwinagbudumeh@gmail.com>
2024-10-17 22:43:41 +0100

    testing deployment

--- src/HomeContentData/data.js
+++ src/HomeContentData/data.js
@@ -1,5 +1,6 @@
 import portfolioImage from '../../public/portfolioImage.jpg';
-//import devlyfImage from '../../public/devlyfImage.png';
+import webImage from '../../public/webImage.png';
+
 
 export const items = [
     {
@@ -8,7 +9,7 @@ export const items = [
         desc: "This is a full stack Content Creation Web App built with React.js, Express.js, Node.js and a MySql database.",
         url: "https://www.devlyf.com",
         gitUrl: "https://github.com/Godwin-Agbudumeh/Full-Stack-Content-Creation-Web-App-using-React.js-Express.js-Node.js-and-MySql",
-        img: ""
+        img: webImage
     },
     {
         id: 2,
