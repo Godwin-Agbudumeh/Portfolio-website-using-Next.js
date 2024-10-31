ac79e06d3afdb3622232afe011c52ccc97142ef7
Godwin Agbudumeh <godwinagbudumeh@gmail.com>
2024-10-31 18:57:56 +0100

    modified data.js

--- src/HomeContentData/data.js
+++ src/HomeContentData/data.js
@@ -1,4 +1,4 @@
-import aiImage from '../../public/aiImage.png';
+// import aiImage from '../../public/aiImage.png';
 import portfolioImage from '../../public/portfolioImage.png';
 import webImage from '../../public/webImage.png';
 
@@ -9,7 +9,7 @@ export const items = [
         desc: "This is a full-stack Chat AI app built with React, Express, Node and a MongoDB database.",
         url: "https://godwin-ai-client.devlyf.com",
         gitUrl: "https://github.com/Godwin-Agbudumeh/Full-Stack-Chat-AI-using-React-Express-Node-and-MongoDB",
-        img: aiImage
+        img: portfolioImage
     },
     {
         id: 2,
