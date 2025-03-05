f2b7418daebc29fc07207ef8e12557cd94db4574
Godwin Agbudumeh <godwinagbudumeh@gmail.com>
2025-03-05 19:29:04 -0200

    modified data.js

--- src/HomeContentData/data.js
+++ src/HomeContentData/data.js
@@ -7,7 +7,7 @@ export const items = [
         id: 1,
         title: "Full Stack Content Creation Web App",
         desc: "This is a full-stack Content Creation Web App built with React, Express, Node and a MySQL database.",
-        url: "https://www.devlyf.com",
+        url: "https://godwin-content-site.vercel.app",
         gitUrl: "https://github.com/Godwin-Agbudumeh/Full-Stack-Content-Creation-Web-App-using-React.js-Express.js-Node.js-and-MySql",
         img: webImage
     },
